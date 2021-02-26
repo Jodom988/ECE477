dd8d6791edbace77a46d0204d0c86ad7b16a3982
jodom988 <jdomajors@gmail.com>
2021-02-26 18:29:48 -0500

    updating

--- laser_detector/camera_test/main.py
+++ laser_detector/camera_test/main.py
@@ -1,12 +1,12 @@
 from picamera import PiCamera
 
 def main():
-	cam = PiCameracamera()
+	cam = PiCamera()
 	cam.resolution = (1064, 768)
 	cam.start_preview()
 
 	sleep(2)
-	cam.capture("~/Desktop/tmp.jpg")
+	cam.capture("~/Desktop/tmp.jp")
 
 if __name__ == '__main__':
 	main()
