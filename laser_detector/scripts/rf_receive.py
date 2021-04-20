43ea3ee09f42afea4c4a8d1561cb0639488e8095
Josh <jdomajors@gmail.com>
2021-04-19 22:55:45 -0400

    laser detector code ready for integration testing

--- laser_detector/scripts/rf_receive.py
+++ laser_detector/scripts/rf_receive.py
@@ -10,35 +10,27 @@ if not pi.connected:
 	print("Couldn't connect to Raspberry Pi daemon, exiting...")
 	exit()
 
-send_address = b'\xAA\x44\x33\x22\x11'
+USB_ADDR = b'\xAA\x44\x33\x22\x11'
+LASER_ADDR = b'\x00\x00\x00\x00\x00'
+
+SELF_ADDR = b'\x99\x88\x77\x66\x55'
 
 nrf = NRF24(pi, ce=25, payload_size=RF24_PAYLOAD.DYNAMIC, channel=52, data_rate=RF24_DATA_RATE.RATE_2MBPS, pa_level=RF24_PA.MAX)
 
-nrf.set_address_bytes(len(send_address))
+nrf.set_address_bytes(len(LASER_ADDR))
 nrf.set_retransmission(15, 15)
 
-
-nrf.open_writing_pipe(send_address)
-
-time.sleep(1)
+nrf.open_reading_pipe(RF24_RX_ADDR.P2,SELF_ADDR)
 
 nrf.reset_packages_lost()
 
-print("About to send:")
 nrf._nrf_write_reg(NRF24.DYNPD, 0x3F)
-nrf._nrf_write_reg(NRF24.FEATURE, 0x6)
+nrf._nrf_write_reg(NRF24.FEATURE, 0x4)
+
 nrf.show_registers()
 
-payload = b'From Josh\x00'
-nrf.send(payload)
-
-time.sleep(1)
-
-if nrf.get_packages_lost() == 1:
-	print("1 NOT RECEIVED")
-else:
-	print("Works!")
-
-print("After sending:")
-nrf.show_registers()
-
+while True:
+	if nrf.data_ready():
+		print("Data ready!")
+		nrf.show_registers()
+		print(str(nrf.get_payload()))
